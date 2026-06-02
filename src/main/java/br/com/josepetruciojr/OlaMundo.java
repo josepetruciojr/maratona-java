100325ecc52760841284cf05fbdd420451f092c5
josepetruciojr <josepetruciodev@gmail.com>
2026-06-02 18:41:30 -0300

    First Commit

---
+++ src/main/java/br/com/josepetruciojr/OlaMundo.java
@@ -0,0 +1,17 @@
+package br.com.josepetruciojr;
+// para realizar um comentario javadoc /** + enter.
+/**
+ * Isto é um comentário javadoc.
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+
+        //isto é um comentário!
+        /*
+        iso é um comentário de bloco
+         */
+
+    }
+}
---
+++ src/main/java/br/com/josepetruciojr/TiposPrimitivos.java
@@ -0,0 +1,41 @@
+package br.com.josepetruciojr;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        /*
+        byte, short, int, long.
+        float, double.
+        char - can be numeric or can be a Unicode ASCII Table.
+        boolean - true or false.
+         */
+
+        int number = 1;
+        double decimalNumber = 20;
+        float floatNumber = 2000.0f;
+        long longNumber = 1000000L;
+        byte byteNumber = -128;
+        short shortNumber = 2;
+
+        char character = 'c';
+        boolean condition = true;
+
+        System.out.println("A idade "+number+" ano.");
+        System.out.println(character);
+        System.out.println(floatNumber);
+
+        //Casting
+
+        longNumber = (long) 155.33;
+        System.out.println(longNumber);
+
+        //String is a reference type and a Wrapper Class.
+
+        String text = "Isto é um grande texto e pode ser colocado aqui";
+        var textTwo = "Isto é um texto";
+
+        System.out.println(text);
+        System.out.println(textTwo);
+
+
+    }
+}
