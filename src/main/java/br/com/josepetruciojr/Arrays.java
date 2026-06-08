1cacce57c9da9f04822352110755403493d25095
josepetruciojr <josepetruciodev@gmail.com>
2026-06-08 01:24:49 -0300

    treinando arrays

--- src/main/java/br/com/josepetruciojr/Arrays.java
+++ src/main/java/br/com/josepetruciojr/Arrays.java
@@ -3,9 +3,31 @@ package br.com.josepetruciojr;
 public class Arrays {
     public static void main(String[] args) {
 
+        testeArray();
+
+
+    }
+
+    public static void testeArray(){
         int[] age = new int[3];
         for (int i = 0; i < age.length; i++){
-            System.out.println(age[i]);
+            age[i] = i;
+            System.out.println("Age: " + age[i]);
         }
+
+        String word = "Anaptalabobiparalelonologramation";
+        char[] chars = word.toCharArray();
+
+        int numberOfChars= 0;
+
+        for(char c : chars){
+            System.out.println(c);
+            numberOfChars++;
+        }
+
+        System.out.println("Apos o char para testar se funciona: " + numberOfChars);
+
+        numberOfChars = word.length();
+        System.out.println("Assim também funciona: " + numberOfChars);
     }
 }
