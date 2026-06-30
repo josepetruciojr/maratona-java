b139ccd9473027194c0635d05751e43ba942b584
josepetruciojr <josepetruciodev@gmail.com>
2026-06-30 00:16:43 -0300

    terminando a introdução a criação de objetos

--- src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/application/FuncionarioTeste01.java
+++ src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/application/FuncionarioTeste01.java
@@ -6,7 +6,11 @@ public class FuncionarioTeste01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
 
-        funcionario.mediaSalario(1000.0,1000.0,1000.0);
+        funcionario.nome = "Ana";
+        funcionario.idade = 25;
+        funcionario.salarios = new double[]{2000.0, 2000.0, 2000.0, 2000.0};
+        funcionario.imprime();
+        funcionario.mediaSalario();
 
     }
 }
--- src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/model/Funcionario.java
+++ src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/model/Funcionario.java
@@ -1,5 +1,7 @@
 package br.com.josepetruciojr.javacore.BintroducaoMetodos.model;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class Funcionario {
 
     /*
@@ -9,19 +11,41 @@ public class Funcionario {
 
     crie 02 metodos
     1-imprimir os dados.
-    2-fazer a media dos salario e imprimir o resultado.
+    2-fazer a média dos salário e imprimir o resultado.
+
+        Aprendi que para chamar o metodo length e calcular ele diretamente sem criar uma variavel acontece assim.
+        media / salarios.length funcionou dentro do metodo println, mas não fora dele, foi preciso colocar o /= para chamar a divisão.
+
      */
     public String nome;
     public int idade;
-    public double salario;
+    public double[] salarios;
 
-    public void imprime(){
+    public void imprime() {
+        if (salarios == null) {
+            System.out.println("Nome: " + this.nome +
+                    "\nIdade: " + this.idade);
+            return;
+        }
         System.out.println("Nome: " + this.nome +
-                            " Idade: " + this.idade +
-                            " Salario: " + this.salario);
+                "\nIdade: " + this.idade +
+                "\nSalario: ");
+
+        for (double salario: salarios){
+            System.out.println("Salario: " + salario);
+        }
     }
 
-    public void mediaSalario(double... salarios){
-
+    public void mediaSalario() {
+        if (salarios == null) {
+            System.out.println("Não é possível calcular a média sem valores.");
+            return;
+        }
+        double media = 0;
+        for (double salario : salarios) {
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("Media dos salários: " + media);
     }
 }
