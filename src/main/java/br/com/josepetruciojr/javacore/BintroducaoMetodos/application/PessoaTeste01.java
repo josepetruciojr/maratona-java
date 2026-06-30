a8a032b79167589f7e157d0fe1e8c7ae2007530e
josepetruciojr <josepetruciodev@gmail.com>
2026-06-30 06:09:05 -0300

    Iniciando modificadores de acesso

---
+++ src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/application/PessoaTeste01.java
@@ -0,0 +1,13 @@
+package br.com.josepetruciojr.javacore.BintroducaoMetodos.application;
+
+import br.com.josepetruciojr.javacore.BintroducaoMetodos.model.Pessoa;
+
+public class PessoaTeste01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(-1);
+        pessoa.imprime();
+
+    }
+}
---
+++ src/main/java/br/com/josepetruciojr/javacore/BintroducaoMetodos/model/Pessoa.java
@@ -0,0 +1,38 @@
+package br.com.josepetruciojr.javacore.BintroducaoMetodos.model;
+
+/**
+ * Coesão é o quanto a classe faz, ex: quando uma classe resolve muito problemas ela tem baixa coesão, pois além de repre-
+ * sentar uma entidade do mundo real, ela também possui muito métodos. Por boas prática cada classe deve resolver apenas-
+ * assuntos relacionados a ela mesmo.
+ * Acoplamento é quanto uma classe conhece da outra e o quanto ela depende da outra classe.
+ * Uma das forma de manter o baixo acoplamento é manter os atributos privados.
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0){
+            System.out.println("Idade inválida.");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+}
